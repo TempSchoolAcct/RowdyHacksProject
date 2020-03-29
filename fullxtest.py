8a4d64433377a700d23e9093eaa1c72992be2e98
Abundov <49936643+abundov@users.noreply.github.com>
2020-03-29 02:41:42 -0500

    Create fullxtest.py

---
+++ fullxtest.py
@@ -0,0 +1,31 @@
+import os
+import time
+import sys
+
+hostname=sys.argv[1]
+run="-c run"
+
+os.system("./recon-cli -w pentest -m recon/domains-hosts/hackertarget -c 'options set source {}' {}".format(hostname, run))
+os.system("./recon-cli -w pentest -m recon/domains-hosts/brute_hosts -c 'options set source {}' {}".format(hostname, run))
+os.system("./recon-cli -w pentest -m recon/domains-hosts/threatminer -c 'options set source {}' {}".format(hostname, run))
+os.system("./recon-cli -w pentest -m recon/domains-hosts/certificate_transparency -c 'options set source {}' {}".format(hostname, run))
+os.system("./recon-cli -w pentest -m recon/domains-contacts/whois_pocs -c 'options set source {}' {}".format(hostname, run))
+os.system("./recon-cli -w pentest -m reporting/html -c 'options set customer fullXtest' -c 'options set creator 404TeamNotFound' {}".format(run))
+os.system("./theHarvester.py -d {} -b ALL -f {}".format(hostname, hostname))
+os.system("nmap -A --script vuln --reason {} -oX nmap.xml".format(hostname))
+os.system("xsltproc nmap.xml -o nmap.html")
+os.system("ls -la")
+
+# fullXtest - a web-based, javascript and python powered automated penetration testing tool
+# created by 404TeamNotFound
+
+
+
+
+
+
+
+
+
+
+

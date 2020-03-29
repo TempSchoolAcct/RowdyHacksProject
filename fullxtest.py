bdb521aca067be1454acb53222c42af2234fc76b
Abundov <49936643+abundov@users.noreply.github.com>
2020-03-29 01:12:24 -0500

    Add files via upload

---
+++ fullxtest.py
@@ -0,0 +1,31 @@
+import os
+import time
+import sys
+
+hostname=sys.argv[0] #set variable from cli argument
+run="'-c run'" #set variable to run the recon-ng exploit
+os.system("./recon-cli -w pentest -m recon/domains-hosts/hackertarget -c 'options set source "
+            "{} {}'".format(hostname, run))
+time.sleep(15)
+os.system("./recon-cli -w pentest -m recon/domains-hosts/brute_hosts -c 'options set source "
+            "{} {}'".format(hostname, run))
+time.sleep(15)
+os.system("./recon-cli -w pentest -m recon/domains-hosts/threatminer -c 'options set source "
+            "{} {}'".format(hostname, run))
+time.sleep(15)
+os.system("./recon-cli -w pentest -m recon/domains-hosts/certificate_transparency -c 'options set source "
+            "{} {}'".format(hostname, run))
+time.sleep(15)
+os.system("./recon-cli -w pentest -m recon/domains-contacts/whois_pocs -c 'options set source "
+            "{} {}'".format(hostname, run))
+time.sleep(15)
+os.system("./recon-cli -w pentest -m reporting/html -c 'options set customer fullXtest' -c 'options set creator "
+          "404TeamNotFound' {}'".format(run))
+time.sleep(15)
+os.system("'./theHarvester.py -d $hostname -b ALL -f {}'".format(hostname))
+time.sleep(15)
+os.system("'nmap -A --script vuln --reason $hostname -oX nmap.xml'")
+time.sleep(90)
+os.system("'xsltproc nmap.xml -o nmap.html'")
+os.system(15)
+os.system('ls -la')
